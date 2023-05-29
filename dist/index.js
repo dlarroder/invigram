a68365adaa78c719483ac73655a06d0dbacf43cb
Dale Larroder <dlarroder@gmail.com>
2023-05-29 22:07:20 +0800

    fix: add new url

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,27 @@
 var magic;
 (function (magic) {
-    var isActive = false;
+    var isActive = true;
     chrome.browserAction.setBadgeBackgroundColor({
-        color: isActive ? "#00FF00" : "#880808",
+        color: isActive ? '#00FF00' : '#880808',
     });
     chrome.browserAction.setBadgeText({
-        text: isActive ? "On" : "Off",
+        text: isActive ? 'On' : 'Off',
     });
     chrome.browserAction.onClicked.addListener(function () {
         isActive = !isActive;
         chrome.browserAction.setBadgeBackgroundColor({
-            color: isActive ? "#00FF00" : "#880808",
+            color: isActive ? '#00FF00' : '#880808',
         });
         chrome.browserAction.setBadgeText({
-            text: isActive ? "On" : "Off",
+            text: isActive ? 'On' : 'Off',
         });
     });
     chrome.webRequest.onBeforeRequest.addListener(function () { return ({
         cancel: isActive,
-    }); }, { urls: ["*://*.instagram.com/api/v1/stories/reel/seen*"] }, ["blocking"]);
+    }); }, {
+        urls: [
+            '*://*.instagram.com/api/v1/stories/reel/seen*',
+            '*://*.instagram.com/api/graphql*',
+        ],
+    }, ['blocking']);
 })(magic || (magic = {}));
--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
 namespace magic {
-  let isActive = false;
+  let isActive = true;
 
   chrome.browserAction.setBadgeBackgroundColor({
-    color: isActive ? "#00FF00" : "#880808",
+    color: isActive ? '#00FF00' : '#880808',
   });
 
   chrome.browserAction.setBadgeText({
-    text: isActive ? "On" : "Off",
+    text: isActive ? 'On' : 'Off',
   });
 
   chrome.browserAction.onClicked.addListener(() => {
     isActive = !isActive;
 
     chrome.browserAction.setBadgeBackgroundColor({
-      color: isActive ? "#00FF00" : "#880808",
+      color: isActive ? '#00FF00' : '#880808',
     });
 
     chrome.browserAction.setBadgeText({
-      text: isActive ? "On" : "Off",
+      text: isActive ? 'On' : 'Off',
     });
   });
 
@@ -25,7 +25,12 @@ namespace magic {
     () => ({
       cancel: isActive,
     }),
-    { urls: ["*://*.instagram.com/api/v1/stories/reel/seen*"] },
-    ["blocking"]
+    {
+      urls: [
+        '*://*.instagram.com/api/v1/stories/reel/seen*',
+        '*://*.instagram.com/api/graphql*',
+      ],
+    },
+    ['blocking']
   );
 }
