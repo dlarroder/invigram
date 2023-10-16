0df71d26509341c225a21e0a47a01700f8496721
Dale Larroder <dlarroder@gmail.com>
2023-10-16 18:49:00 +0800

    chore: move to bun

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,36 @@
+// index.ts
 var magic;
-(function (magic) {
-    var isActive = true;
+(function(magic) {
+  let isActive = true;
+  chrome.browserAction.setBadgeBackgroundColor({
+    color: isActive ? "#00FF00" : "#880808"
+  });
+  chrome.browserAction.setBadgeText({
+    text: isActive ? "On" : "Off"
+  });
+  chrome.browserAction.onClicked.addListener(() => {
+    isActive = !isActive;
     chrome.browserAction.setBadgeBackgroundColor({
-        color: isActive ? '#00FF00' : '#880808',
+      color: isActive ? "#00FF00" : "#880808"
     });
     chrome.browserAction.setBadgeText({
-        text: isActive ? 'On' : 'Off',
+      text: isActive ? "On" : "Off"
     });
-    chrome.browserAction.onClicked.addListener(function () {
-        isActive = !isActive;
-        chrome.browserAction.setBadgeBackgroundColor({
-            color: isActive ? '#00FF00' : '#880808',
-        });
-        chrome.browserAction.setBadgeText({
-            text: isActive ? 'On' : 'Off',
-        });
-    });
-    chrome.webRequest.onBeforeRequest.addListener(function (details) {
-        if (details && details.requestBody && details.requestBody.formData) {
-            var formData = details.requestBody.formData;
-            var statusBlocking = formData.fb_api_req_friendly_name.includes('PolarisAPIReelSeenMutation');
-            return { cancel: isActive && statusBlocking };
-        }
-        else {
-            return { cancel: false };
-        }
-    }, {
-        urls: [
-            '*://*.instagram.com/api/v1/stories/reel/seen*',
-            '*://*.instagram.com/api/graphql*',
-        ],
-    }, ['blocking', 'requestBody']);
+  });
+  chrome.webRequest.onBeforeRequest.addListener((details) => {
+    if (details && details.requestBody && details.requestBody.formData) {
+      const {
+        requestBody: { formData }
+      } = details;
+      const statusBlocking = formData.fb_api_req_friendly_name.includes("PolarisAPIReelSeenMutation");
+      return { cancel: isActive && statusBlocking };
+    } else {
+      return { cancel: false };
+    }
+  }, {
+    urls: [
+      "*://*.instagram.com/api/v1/stories/reel/seen*",
+      "*://*.instagram.com/api/graphql*"
+    ]
+  }, ["blocking", "requestBody"]);
 })(magic || (magic = {}));
